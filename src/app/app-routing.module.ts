1b949103d100a419c66c1ca4531e894b3e3c672c
MattCowin <cowinmatt@gmail.com>
2019-06-11 13:31:25 -0400

    Started to add nav itmes

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from '../app/login/login.component';
+import { LogoutComponent } from '../app/logout/logout.component';
+
+const routes: Routes = [
+  {
+    path: 'login', component: LoginComponent,
+  },
+  {
+    path: 'logout', component: LogoutComponent,
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
